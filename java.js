848240ae3d239109bbef8d08b153d9d458dc1a84
Dimdinobones <z5590034@ad.unsw.edu.au>
2025-08-11 20:20:18 +1000

    Add files via upload

---
+++ java.js
@@ -0,0 +1,95 @@
+
+ function Megalosaurus006() {
+    mywindow = window.open("Megalosaurus006.html", "mywindow", "location=1,status=1,scrollbars=1,  width=600,height=700");
+    mywindow.moveTo(0, 0);
+}
+
+function transition1() {
+    var img = document.getElementById("transition1");
+    img.style.position = "relative";
+    img.style.width = "50%";
+    img.style.height = "30%";
+    img.style.transition = "width 2s, height 2s, opacity 2s";
+    img.style.zIndex = "0";
+}
+
+function Megalosaurus006() {
+    mywindow = window.open("Megalosaurus006.html", "mywindow", "location=1,status=1,scrollbars=1,  width=600,height=700");
+    mywindow.moveTo(0, 0);
+}
+
+
+
+ function Iguanodon027() {
+    mywindow = window.open("Iguanodon027.html", "mywindow", "location=1,status=1,scrollbars=1,  width=600,height=700");
+    mywindow.moveTo(0, 0);
+}
+
+function transition1() {
+    var img = document.getElementById("transition1");
+    img.style.position = "relative";
+    img.style.width = "50%";
+    img.style.height = "30%";
+    img.style.transition = "width 2s, height 2s, opacity 2s";
+    img.style.zIndex = "0";
+}
+
+function Iguanodon027() {
+    mywindow = window.open("Iguanodon027.html", "mywindow", "location=1,status=1,scrollbars=1,  width=600,height=700");
+    mywindow.moveTo(0, 0);
+}
+
+
+
+ function Cetiosaurus014() {
+    mywindow = window.open("Cetiosaurus014.html", "mywindow", "location=1,status=1,scrollbars=1,  width=600,height=700");
+    mywindow.moveTo(0, 0);
+}
+
+function transition1() {
+    var img = document.getElementById("transition1");
+    img.style.position = "relative";
+    img.style.width = "50%";
+    img.style.height = "30%";
+    img.style.transition = "width 2s, height 2s, opacity 2s";
+    img.style.zIndex = "0";
+}
+
+function Cetiosaurus014() {
+    mywindow = window.open("Cetiosaurus014.html", "mywindow", "location=1,status=1,scrollbars=1,  width=600,height=700");
+    mywindow.moveTo(0, 0);
+}
+
+
+
+ function Hylaeosaurus003() {
+    mywindow = window.open("Hylaeosaurus003.html", "mywindow", "location=1,status=1,scrollbars=1,  width=600,height=700");
+    mywindow.moveTo(0, 0);
+}
+
+function transition1() {
+    var img = document.getElementById("transition1");
+    img.style.position = "relative";
+    img.style.width = "50%";
+    img.style.height = "30%";
+    img.style.transition = "width 2s, height 2s, opacity 2s";
+    img.style.zIndex = "0";
+}
+
+function Hylaeosaurus003() {
+    mywindow = window.open("Hylaeosaurus003.html", "mywindow", "location=1,status=1,scrollbars=1,  width=600,height=700");
+    mywindow.moveTo(0, 0);
+}
+
+
+function playAudio2() {
+
+    var Audio = document.getElementById("Audio2");
+
+    if (Audio.paused) {
+        Audio.play();
+        Audio.volume = 0.4;
+    } else {
+        Audio.pause();
+    }
+}
